bdf13152dfc879012f8ee119ea8348d357e79f1f
susuyan <susuyan@163.com>
2026-04-18 10:24:10 +0800

    fix: download all images locally, disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "img.k3cdn.com",
-      },
-      {
-        protocol: "http",
-        hostname: "shop10612182776.juyi5.cn",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
